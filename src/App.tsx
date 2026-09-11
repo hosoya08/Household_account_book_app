b92ff18c61a3c90f1e49c61c8ff28879f471aee3
hosoya08 <118666052+hosoya08@users.noreply.github.com>
2026-09-11 13:51:40 +0900

    [update] routerを使って画面遷移できるように変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import "./App.css"
-import "./assets/reset.css";
-import "./assets/main.css";
+import "./assets/reset.css"
+import "./assets/main.css"
+import { Routes, Route } from "react-router-dom"
 import { Dashboard } from "./page/DashboardPage"
 import Header from "./components/Header"
+import ExpensesPage from "./page/ExpensesPage"
 
-function App() {
+const App: React.FC = () => {
     return (
-        <div className="content">
+        <div className='content'>
             <Header />
-            <Dashboard />
+            <Routes>
+                <Route path='/' element={<Dashboard />} />
+                <Route path='/expenses' element={<ExpensesPage />} />
+            </Routes>
         </div>
     )
 }
